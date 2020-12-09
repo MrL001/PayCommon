506ff91eb35fe1f892cadf99c88072a7dd2f9bbb
MrL001 <499827593@qq.com>
2020-12-09 14:23:02 +0800

    项目重命名

--- src/main/java/com/hntyy/ThirdProjectApplication.java
+++ src/main/java/com/hntyy/ThirdProjectApplication.java
@@ -1,25 +1,18 @@
 package com.hntyy;
 
-import com.hntyy.common.RedisUtil;
 import com.hntyy.common.SpringUtil;
-import com.hntyy.entity.mjjzxyh.SensitiveWordsEntity;
-import com.hntyy.service.mjjzxyh.SensitiveWordsService;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-import javax.annotation.Resource;
-import java.util.List;
-
 @SpringBootApplication
 @MapperScan("com.hntyy.mapper")  //使用MapperScan批量扫描所有的Mapper接口；
 @Import(SpringUtil.class)
-public class PayCommonApplication {
+public class ThirdProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PayCommonApplication.class, args);
+        SpringApplication.run(ThirdProjectApplication.class, args);
     }
 
 }
