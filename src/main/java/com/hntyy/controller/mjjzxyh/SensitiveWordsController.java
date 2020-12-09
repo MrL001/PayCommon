4f6bd6ff0971774c586ee026b66dadc129436b78
MrL001 <499827593@qq.com>
2020-12-09 11:46:32 +0800

    敏感词管理完成增删改查

--- src/main/java/com/hntyy/controller/mjjzxyh/SensitiveWordsController.java
+++ src/main/java/com/hntyy/controller/mjjzxyh/SensitiveWordsController.java
@@ -13,11 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
